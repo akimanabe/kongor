4df992d0bfca95b7b260b6a2eb65dad62fcbb9de
Akihiro Manabe <amanabe@affrc.go.jp>
2020-11-26 10:25:03 +0900

    :white_check_mark: Update tests

--- tests/testthat/test-create_mixgauss.R
+++ tests/testthat/test-create_mixgauss.R
@@ -1,14 +1,57 @@
 context("create_mixgauss")
 
 test_that("function exists", {
-  expect_is(cteate_mixgauss, "function")
+  expect_is(create_mixgauss, "function")
 })
 
 test_that("function works", {
-  expect_equal(length(create_mixgauss(
-    n = list(100, 100, 50),
-    mean = list(20, 50, 100),
-    mu = list(10, 20, 20)
-    )), sum(100, 100, 50)
+  age <- c(1, 2, 3)
+  n   <- c(100, 100, 50)
+  mu  <- c(20, 50, 100)
+  sd  <- c(10, 20, 20)
+
+  # tibble column number equals to 5
+  expect_equal(
+    create_mixgauss(age, n, mu, sd) %>%
+      ncol(), 5
+  )
+
+  # generated length counts equals to sum of n
+  expect_equal(
+    create_mixgauss(age, n, mu, sd) %>%
+      nrow(),
+    sum(n)
+  )
+})
+
+test_that("gaussian mean is adequate", {
+  age <- c(1, 2, 3)
+  n   <- c(100, 100, 50)
+  mu  <- c(20, 50, 100)
+  sd  <- c(10, 20, 20)
+
+  expect_equal(
+    create_mixgauss(age, n, mu, sd) %>%
+      dplyr::filter(age == 1) %>%
+      dplyr::pull(length) %>%
+      mean(),
+    mu[1],
+    tolerance = 1
+  )
+  expect_equal(
+    create_mixgauss(age, n, mu, sd) %>%
+      dplyr::filter(age == 2) %>%
+      dplyr::pull(length) %>%
+      mean(),
+    mu[2],
+    tolerance = 1
+  )
+  expect_equal(
+    create_mixgauss(age, n, mu, sd) %>%
+      dplyr::filter(age == 3) %>%
+      dplyr::pull(length) %>%
+      mean(),
+    mu[3],
+    tolerance = 1
   )
 })
